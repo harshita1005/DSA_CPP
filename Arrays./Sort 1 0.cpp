eadfc9ba593157b1ac9caae5e47a4d1f11bcfdbb
Harshita Totala <88829866+harshita1005@users.noreply.github.com>
2022-12-27 08:21:00 +0530

    You are given an array of 0s and 1s in random order. Segregate 0s on left side and 1s on right side of the array [Basically you have to sort the array].

---
+++ Arrays./Sort 1 0.cpp
@@ -0,0 +1,49 @@
+//You are given an array of 0s and 1s in random order. Segregate 0s on left side and 1s on right side of the array [Basically you have to sort the array]. 
+
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void sortOne(int arr[], int n) {
+
+    int left = 0, right = n-1;
+
+    while(left < right) {
+
+        while(arr[left] == 0 && left < right )  {
+            left++;
+        }
+
+        while(arr[right]==1 && left < right){
+            right--;
+        }
+
+        //agar yha pohoch gye ho, iska matlab
+        //arr[left]==1 and arr[right]==0
+        if(left<right)
+        {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+
+}
+
+int main() {
+
+    int arr[8] = {1,1,0,0,0,0,1,0};
+
+    sortOne(arr, 8);
+    printArray(arr, 8);
+
+
+    return 0;
+}
+
