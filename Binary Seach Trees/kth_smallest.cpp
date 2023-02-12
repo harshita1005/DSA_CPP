52d25e23292f66abbb2e98a173cedb4e8aa4b392
Harshita Totala <88829866+harshita1005@users.noreply.github.com>
2023-02-12 16:51:45 +0530

    Create kth_smallest.cpp

---
+++ Binary Seach Trees/kth_smallest.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    int solve(TreeNode* root,int &i, int k){
+        
+        if(root==NULL){
+            return -1;
+        }
+        //left
+        int left=solve(root->left,i,k);
+        if(left!=-1){
+            return left;
+        }
+        i++;
+        //node
+        if(i==k){
+            return root->val;
+        }
+        
+        //right
+        return solve(root->right,i,k);
+        
+        
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int i=0;
+        return solve(root,i,k);
+    }
+};
