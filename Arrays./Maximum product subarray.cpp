5c921a32a147214b21a08804df9ebe3af90132b0
Harshita Totala <88829866+harshita1005@users.noreply.github.com>
2023-09-15 14:24:07 +0530

    Create Maximum product subarray.cpp

---
+++ Arrays./Maximum product subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        
+      int maxi=nums[0];
+        int mini=nums[0];
+        int ans=nums[0];
+        
+        for(int i=1;i<nums.size();i++){
+            
+            if(nums[i]<0){
+                swap(maxi,mini);
+            }
+            if(nums[i]==0){
+                maxi=1;
+                mini=1;
+                
+            }
+            maxi=max(nums[i],maxi*nums[i]);
+            mini=min(nums[i],mini*nums[i]);
+            ans=max(ans,maxi);
+        }
+        return ans;
+    }
+};
