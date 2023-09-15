6d907c65b28e2097952d3d9ea12b087afb74ad14
Harshita Totala <88829866+harshita1005@users.noreply.github.com>
2023-09-15 14:08:33 +0530

    Create Kadane's Algorithm.cpp

---
+++ Arrays./Kadane's Algorithm.cpp
@@ -0,0 +1,23 @@
+//Kadane's algorithm - Maximum Subarray
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum=0;
+        int maxi=nums[0];
+
+        for(int i=0; i<nums.size();i++){
+            sum=sum+nums[i];
+
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+
+    }
+};
